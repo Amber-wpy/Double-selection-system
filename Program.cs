df42134d2d5fc524e8ce44d64594e80989124445
wupin <943232356@qq.com>
2020-03-08 13:00:21 +0800

    看看能不能合并到master

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 namespace Double_selection_system
 {
     static class Program
-    {   ///test
+    {   ///test1
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
